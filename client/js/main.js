4cd946dce7e1647429d25c5713582d5adb0a875f
Steve Drees <steve.drees@live.com>
2015-05-14 04:51:30 +0000

    Everything working 5/14/15-0100

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,28 @@
+var socket = io.connect();
+
+        window.onload = function(){
+            var location = NoGPS.getLocation(myCallback);
+            //console.log(location)
+        };
+
+    
+        function myCallback(location) {
+            
+            var location = JSON.stringify(location);
+            var data = JSON.parse(location);
+
+            
+            var lat = data['latitude'];
+            var long = data['longitude'];
+            
+            socket.emit('location', {long:long, lat:lat});
+
+        }
+        
+/* Angulare Code Below here */        
+
 angular.module('penny', ['ngRoute'])
 .controller('userInfo', userInfo);
-var socket = io.connect();
 
 var FireRef = new Firebase('https://pennypincher.firebaseio.com/');
 
@@ -24,14 +46,11 @@ function userInfo($scope){
           $scope.loginValue = true;
           //FireRef.set({authData: authData});
           console.log("Authenticated successfully with payload:", authData);
-         socket.emit('loginData', {data: authData});
-        //console.log($scope.profileURL);
+          socket.emit('loginData', {data: authData});
+          //console.log($scope.profileURL);
         window.location.href = "https://pennypincher-wsdrees.c9.io/";
-
         }
-      
       });
-      
     };
       
 function callBack(authData){
@@ -42,8 +61,7 @@ function callBack(authData){
       $scope.loginValue = false;
       
     }
-  }
-
+  }//END callBack()
 
 $scope.logout = function(){
         
@@ -51,7 +69,6 @@ $scope.logout = function(){
         FireRef.onAuth(callBack);
         window.location.href = "https://pennypincher-wsdrees.c9.io/";
   };
-  
 }
 
 $('#messageInput').keypress(function (e) {
@@ -64,3 +81,34 @@ $('#messageInput').keypress(function (e) {
           
         }
 });
+
+function getLocation() {
+    
+        //navigator.geolocation.getCurrentPosition(showPosition);
+        var location = navigator.geolocation.getCurrentPosition(showPosition);
+        
+        //navigator.geolocation.getCurrentPosition(showPosition);
+        //console.log(navigator.geolocation.getCurrentPosition(showPosition));
+
+}//END getLocation()
+
+        // var lat = position.coords.latitude;
+        // var long = position.coords.longitude;
+        // console.log(long, lat);
+        // socket.emit('location', { long:long, lat:lat});
+        
+
+function getDeals(yelp){
+    
+    var title = yelp.deals.title;
+    var start = yelp.deals.time_start; 
+    var end = yelp.deals.time_end;
+    var popular = yelp.deals.is_popular;
+    var url = yelp.deals.url;
+    var image = yelp.deals.image_url;
+    
+    var dealOptions = { url: 'http://api.yelp.com/v2/businesses/'+title['title']+'&amp;start='+start['start']+'&amp;end='+end['end']+'&amp;popular='+popular['popular']+'&amp;url='+url['url']+'&amp;image'+image['image'] };
+    
+    console.log(dealOptions);
+    
+}//END getDeals()
--- server.js
+++ server.js
@@ -5,13 +5,15 @@
 //
 var http = require('http');
 var path = require('path');
-//var angular = require("angular");
+var yelp = require('yelp');
 var serveAngular = require('serve-angular');
 var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 var Firebase = require("firebase");
 var FireRef = new Firebase("https://pennypincher.firebaseio.com/");
+var bodyParser = require("body-parser");
+var curl = require("curlrequest");
 
 //
 // ## SimpleServer `SimpleServer(obj)`
@@ -24,14 +26,57 @@ var server = http.createServer(router);
 var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(bodyParser.urlencoded({extended: false}));
 router.set('view engine', 'ejs'); //set the engine to ejs so you can actually view it
 
 //ANYTHING YOU WANT TO SHOW ON THE HOMEPAGE(PROCESS) DATA
 router.get("/",function(req,res){
+      res.render("index");
+});
+
+router.get('/yelp', function(){
+  
+    io.on('connection', function (socket) {
     
-  res.render("index");
+        socket.on('location', function (data) {
+
+            var options = { url: 'http://api.yelp.com/business_review_search?term=chinese&amp;lat='+data['lat']+'&amp;long='+data['long']+'&amp;radius=10&amp;limit=5&amp;ywsid=YTfBsfvQR_JdGPmD4SqTWA', include: true };
+              
+            curl.request(options, function(err, yelpResults) {
+              
+            socket.emit('yelpData', {yelp:yelpResults});
+              
+            });
+              
+        });
+        
+  socket.on('yelpData', function (data) {
+        console.log(data);
+        //KEEP AT THE BOTTOM
+        res.send("returned", {yelpResults:data});
+        
+  });
+        
+});
   
 });
+
+router.get("/profile", function(req,res){
+  
+  console.log("profile");
+  
+  //KEEP AT BOTTOM
+  res.render("profile");
+});
+
+router.post("/processSearch",function(req,res){
+  var foodType = req.body.foodType;
+  
+  console.log(foodType);
+
+});
+
+
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
